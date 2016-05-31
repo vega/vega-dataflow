081680aca4580aeeeb43d90a311c4a27bcc8db57
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-05-31 13:17:31 -0700

    Update and test Lookup.

--- src/data/Lookup.js
+++ src/data/Lookup.js
@@ -8,8 +8,8 @@ import {inherits} from '../util/Functions';
  * @param {object} params - The parameters for this operator.
  * @param {object} params.index - The lookup table.
  * @param {Array<function(object): *} params.keys - The lookup keys.
- * @param {Array<string>} params.fields - The per-key output fields to write.
- * @param {*} [params.default] - A default value to use if lookup failes.
+ * @param {Array<string>} params.as - The per-key output field names.
+ * @param {*} [params.default] - A default value to use if lookup fails.
  */
 export default function Lookup(params) {
   Transform.call(this, {}, params);
@@ -18,17 +18,18 @@ export default function Lookup(params) {
 var prototype = inherits(Lookup, Transform);
 
 prototype.transform = function(_, pulse) {
-  var fields = _.fields,
+  var out = pulse,
+      as = _.as,
       index = _.index,
       keys = _.keys,
       defaultValue = _.default==null ? null : _.default,
-      reset = _.modified('index'),
+      reset = _.modified(),
       flag = pulse.ADD,
       set, key, field, mods;
 
   if (keys.length === 1) {
     key = keys[0];
-    field = fields[0];
+    field = as[0];
     set = function(t) {
       var v = index[key(t)];
       t[field] = v==null ? defaultValue : v;
@@ -37,18 +38,19 @@ prototype.transform = function(_, pulse) {
     set = function(t) {
       for (var i=0, n=keys.length, v; i<n; ++i) {
         v = index[keys[i](t)];
-        t[fields[i]] = v==null ? defaultValue : v;
+        t[as[i]] = v==null ? defaultValue : v;
       }
     };
   }
 
   if (reset) {
     flag = pulse.SOURCE;
+    out = pulse.fork(pulse.ALL).reflow();
   } else {
     mods = keys.some(function(k) { return pulse.modified(k.fields); });
-    flag = pulse.ADD |  (mods ? pulse.MOD : 0);
+    flag = pulse.ADD | (mods ? pulse.MOD : 0);
   }
   pulse.visit(flag, set);
 
-  return pulse.modifies(fields);
+  return out.modifies(as);
 };
---
+++ test/data/lookup-test.js
@@ -0,0 +1,53 @@
+var tape = require('tape'),
+    dataflow = require('../../');
+
+tape("Lookup looks up matching tuples", function(test) {
+  var lut = [
+    {"id": 1, "value": "foo"},
+    {"id": 3, "value": "bar"},
+    {"id": 5, "value": "baz"},
+  ].map(dataflow.Tuple.ingest);
+
+  var data = [
+    {"id": 0, "x": 5, "y": 1},
+    {"id": 1, "x": 3, "y": 5},
+    {"id": 2, "x": 1, "y": 5},
+    {"id": 3, "x": 3, "y": 3}
+  ].map(dataflow.Tuple.ingest);
+
+  var uv = function(t) { return t.u.value; }, // field('u.value')
+      vv = function(t) { return t.v.value; }, // field('v.value')
+      id = dataflow.field('id'),
+      x = dataflow.field('x'),
+      y = dataflow.field('y'),
+      df = new dataflow.Dataflow(),
+      c0 = df.add(dataflow.Collect),
+      hi = df.add(dataflow.HashIndex, {field:id, pulse:c0}),
+      c1 = df.add(dataflow.Collect),
+      lk = df.add([x,y]),
+      lu = df.add(dataflow.Lookup, {index:hi, keys:lk, as:['u','v'], pulse:c1});
+
+  df.run(); // initialize
+
+  // add lookup table
+  df.nextPulse.add = lut;
+  df.touch(c0).run();
+  test.equal(Object.keys(hi.value).length, 3);
+
+  // add primary data
+  df.nextPulse.add = data;
+  df.touch(c1).run();
+  var p = lu.pulse.add;
+  test.equal(p.length, 4);
+  test.deepEqual(p.map(uv), ['baz', 'bar', 'foo', 'bar']);
+  test.deepEqual(p.map(vv), ['foo', 'baz', 'baz', 'bar']);
+
+  // swap lookup keys
+  df.update(lk, [y,x]).run();
+  p = lu.pulse.mod;
+  test.equal(p.length, 4);
+  test.deepEqual(p.map(vv), ['baz', 'bar', 'foo', 'bar']);
+  test.deepEqual(p.map(uv), ['foo', 'baz', 'baz', 'bar']);
+
+  test.end();
+});
