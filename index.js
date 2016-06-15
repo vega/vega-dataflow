f39ba05946356c746a495e3837982e98dc589749
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-06-15 11:43:36 -0700

    Export all utilities.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 export {version} from './build/package';
 
 // Utilities
-export {accessor, field, compare} from './src/util/Functions';
-export {error, info, warn, logLevel} from './src/util/Errors';
+export * from './src/util/Functions';
+export * from './src/util/Objects';
+export * from './src/util/Errors';
+export * from './src/util/Random';
 export {default as UniqueList} from './src/util/UniqueList';
-export {normal} from './src/util/Random';
 
 // Core Runtime
 import * as Tuple from './src/Tuple'; export {Tuple};
