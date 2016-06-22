94df4c1dcecdb849ebb45061ad19bf5947d6a7db
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-06-21 21:43:17 -0700

    Clean up Apply transform.

--- src/data/Apply.js
+++ src/data/Apply.js
@@ -6,7 +6,7 @@ import {inherits} from '../util/Functions';
  * @constructor
  * @param {object} params - The parameters for this operator.
  * @param {function(object): *} params.apply - The function to apply to each tuple.
- * @param {string} params.as - the field name under which to store the result.
+ * @param {string} params.as - The field name under which to store the result.
  */
 export default function Apply(params) {
   Transform.call(this, null, params);
@@ -17,7 +17,7 @@ var prototype = inherits(Apply, Transform);
 prototype.transform = function(_, pulse) {
   var func = _.apply,
       field = _.as,
-      flags;
+      mod;
 
   function set(t) {
     t[field] = func(t, _);
@@ -25,13 +25,10 @@ prototype.transform = function(_, pulse) {
 
   if (_.modified()) {
     // parameters updated, need to reflow
-    pulse
-      .materialize()
-      .visit(pulse.ADD_MOD, set)
-      .visit(pulse.REFLOW, function(t) { set(t); pulse.mod.push(t); });
+    pulse.materialize().reflow().visit(pulse.SOURCE, set);
   } else {
-    flags = pulse.ADD | (pulse.modified(func.fields) ? pulse.MOD : 0);
-    pulse.visit(flags, set);
+    mod = pulse.modified(func.fields);
+    pulse.visit(mod ? pulse.ADD_MOD : pulse.ADD, set);
   }
 
   return pulse.modifies(field);
