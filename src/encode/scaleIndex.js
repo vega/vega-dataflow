b5562c6e65f337e94e193c4fce687c8292a04641
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-27 09:32:32 -0700

    Improve index scale, add tests.

--- src/encode/scaleIndex.js
+++ src/encode/scaleIndex.js
@@ -1,41 +1,64 @@
-import {scaleOrdinal, scaleLinear, scaleSequential} from 'd3-scale';
-import {range} from 'd3-array';
+import {scaleLinear, scaleSequential} from 'd3-scale';
 
 export default function index(scheme) {
-  var ordinal = scaleOrdinal(),
-      interpolate = scheme ? scaleSequential(scheme) : scaleLinear();
+  var domain = [],
+      length = 0,
+      lookup = {},
+      interp = scheme ? scaleSequential(scheme) : scaleLinear();
 
   function scale(_) {
-    return interpolate(ordinal(_));
+    var i = lookup.hasOwnProperty(_) && lookup[_];
+    if (i) return interp(i);
   }
 
-  scale.ordinal = ordinal;
-  scale.interp = interpolate;
-
   scale.domain = function(_) {
-    if (!arguments.length) return ordinal.domain();
-    ordinal.domain(_);
-    ordinal.range(range(0, _.length));
-    interpolate.domain([0, _.length-1]);
+    if (!arguments.length) return domain.slice();
+    domain = _.slice();
+    length = domain.length;
+    lookup = {};
+    for (var i=0; i<length;) lookup[domain[i]] = ++i;
+    interp.domain([1, length]);
     return scale;
   };
 
-  scale.invert = function(_) {
-    return ordinal.domain()[~~interpolate.invert(_)];
-  };
+  if (!scheme) {
+    // sequential scales do not export an invert method
 
-  (scheme ? ['clamp'] : ['clamp', 'range', 'rangeRound'])
+    scale.invert = function(_) {
+      return domain[interp.invert(_) - 1];
+    };
+
+    scale.invertExtent = function(r0, r1) {
+      if (arguments.length < 2) {
+        return (t = scale.invert(r0)) !== undefined ? [t] : t;
+      }
+
+      var lo = interp.invert(r0) - 1,
+          hi = interp.invert(r1) - 1,
+          t;
+      if (lo > hi) t = lo, lo = hi, hi = t;
+      return (hi < 0 || lo >= length) ? [] : domain.slice(
+        Math.max(0, Math.ceil(lo)),
+        Math.min(length, Math.floor(hi) + 1)
+      );
+    };
+  }
+
+  (scheme
+    ? ['interpolator']
+    : ['interpolate', 'range', 'rangeRound'])
     .forEach(function(method) {
       scale[method] = function() {
-        var r = interpolate[method].apply(null, arguments);
+        var r = interp[method].apply(null, arguments);
         return arguments.length ? scale : r;
       };
     });
 
   scale.copy = function() {
-    return (scheme ? index(scheme) : index().range(interpolate.range()))
-      .domain(ordinal.domain())
-      .clamp(interpolate.clamp());
+    return (scheme
+      ? index(scheme).interpolator(interp.interpolator())
+      : index().interpolate(interp.interpolate()).range(interp.range())
+    ).domain(domain);
   };
 
   return scale;
