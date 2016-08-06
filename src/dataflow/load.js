1ac5fe8cf0aa9f85f8e7fbd84b281f3075f02579
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-06 14:31:18 -0700

    Move from callbacks to promises.

--- src/dataflow/load.js
+++ src/dataflow/load.js
@@ -9,21 +9,17 @@ export function request(target, url, format) {
 
   df._requests = 1 + (df._requests || 0);
 
-  load(url, df._loadopt, function(err, data) {
-    if (!err) {
-      try {
-        df.ingest(target, data, format);
-      } catch (e) { err = e; }
-    }
+  function decrement() {
+    if (--df._requests === 0) df.runAfter(function() { df.run(); });
+  }
 
-    if (err) {
-      df.warn('Loading failed: ' + url, err);
-    }
-
-    if (--df._requests === 0) {
-      df.runAfter(function() { df.run(); });
-    }
-  });
+  load(url, df._loadopt)
+    .then(function(data) { return df.ingest(target, data, format); })
+    .then(decrement)
+    .catch(function(error) {
+      df.warn('Loading failed: ' + url, error);
+      decrement()
+    });
 }
 
 export function ingest(target, data, format) {
