6b935ff1b5238d3c7f5c6da931d1bf01a65cec31
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-29 17:10:34 -0700

    Update Rank transform definition.

--- definitions/Rank.js
+++ definitions/Rank.js
@@ -2,7 +2,7 @@ export default {
   "type": "Rank",
   "metadata": {"modifies": true},
   "params": [
-    { "name": "field", "type": "field", "required": true },
+    { "name": "field", "type": "field" },
     { "name": "normalize", "type": "boolean", "default": false },
     { "name": "as", "type": "string", "default": "rank" }
   ]
